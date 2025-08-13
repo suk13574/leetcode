53c5e47a122aab78c64f3f0056e7334795e361f4
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-08-13 09:56:19 +0900

    Time: 1 ms (87.45%), Space: 7.9 MB (60.52%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.go
+++ 0326-power-of-three/0326-power-of-three.go
@@ -1,11 +1,16 @@
+import "math"
+
 func isPowerOfThree(n int) bool {
 	if n <= 0 {
 		return false
 	}
 
-	for n % 3 == 0 {
-		n = n / 3
+	var max3Pow int64 = 1
+
+	for max3Pow <= math.MaxInt64/3 {
+		max3Pow *= 3
 	}
 
-	return n == 1
+	return max3Pow%int64(n) == 0
+
 }
