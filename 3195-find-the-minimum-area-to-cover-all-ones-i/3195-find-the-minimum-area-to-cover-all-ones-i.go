87b4f35d4393cfb248dcddd6a4e32ab9c797ceb6
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-08-22 09:30:06 +0900

    Time: 304 ms (44.44%), Space: 26.3 MB (66.67%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.go
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.go
@@ -1,23 +1,29 @@
-func minimumArea(grid [][]int) int {
-	x1, y1 := 1001, 1001
-	x2, y2 := -1, -1
+import "math"
 
-	cnt := 0
+func minimumArea(grid [][]int) int {
+	x1, y1 := math.MaxInt, math.MaxInt
+	x2, y2 := math.MinInt, math.MinInt
+	found := false
+
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
-			if grid[i][j] == 0 {
-				continue
+			if grid[i][j] == 1 {
+				if !found {
+					x1, y1, x2, y2 = i, j, i, j
+					found = true
+				} else {
+					x1 = min(x1, i)
+					x2 = max(x2, i)
+					y1 = min(y1, j)
+					y2 = max(y2, j)
+				}
 			}
-			cnt++
-			x1 = min(x1, i)
-			x2 = max(x2, i)
-			y1 = min(y1, j)
-			y2 = max(y2, j)
+
 		}
 	}
 
-	if cnt == 1 {
-		return 1
+	if !found {
+		return 0
 	}
 
 	x := x2 - x1 + 1
