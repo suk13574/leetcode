6efc87a4f64e9b500386ab1e7fa835ecb8e71685
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-08-29 09:36:32 +0900

    Time: 0 ms (100%), Space: 4.1 MB (16.67%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.go
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.go
@@ -1,14 +1,6 @@
 func flowerGame(n int, m int) int64 {
-	evenN, oddN := n/2, n/2
-	evenM, oddM := m/2, m/2
+	evenN, oddN := n/2, (n+1)/2
+	evenM, oddM := m/2, (m+1)/2
 
-	if n%2 == 1 {
-		oddN++
-	}
-	if m%2 == 1 {
-		oddM++
-	}
-
-	return (int64(evenN) * int64(oddM)) + int64(oddN)*int64(evenM)
-
+	return int64(evenN*oddM + oddN*evenM)
 }
