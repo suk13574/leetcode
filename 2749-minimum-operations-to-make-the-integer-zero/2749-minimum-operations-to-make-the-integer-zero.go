beba53e9856f14aa4e2f776e93b8bfcdfbd19db3
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-09-05 10:04:59 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.go
@@ -0,0 +1,24 @@
+func makeTheIntegerZero(num1 int, num2 int) int {
+	countSum := func(num int) int {
+		result := 0
+		for num > 0 {
+			if num&1 == 1 {
+				result++
+			}
+			num = num >> 1
+		}
+
+		return result
+	}
+
+	var leftTerm int
+	for i := 1; i < 61; i++ {
+		leftTerm = num1 - (i * num2)
+
+		if leftTerm > i && countSum(leftTerm) <= i {
+			return i
+		}
+	}
+
+	return -1
+}
