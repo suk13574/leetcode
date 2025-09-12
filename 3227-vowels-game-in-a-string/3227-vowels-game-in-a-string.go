6508867c8a3a239316f43d9bed73f74458d1e5aa
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-09-12 09:25:18 +0900

    Time: 1 ms (63.64%), Space: 8.8 MB (27.27%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.go
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.go
@@ -1,15 +1,8 @@
 func doesAliceWin(s string) bool {
-    isVowel := func(c byte) bool {
-        switch c {
-        case 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u':
-            return true
-        }
-        return false
-    }
-
     for i := 0; i < len(s); i++ {
-        if isVowel(s[i]) {
-            return  true
+        switch s[i] {
+        case 'a', 'e', 'i', 'o', 'u':
+            return true
         }
     }
     return false
