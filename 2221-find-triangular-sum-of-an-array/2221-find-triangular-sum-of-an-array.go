bf35c37d5e5f74f5b7cfd9f77dece0d75da2d718
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-09-30 10:08:32 +0900

    Time: 33 ms (60%), Space: 6.3 MB (60%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.go
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.go
@@ -2,18 +2,10 @@ func triangularSum(nums []int) int {
 	const MOD = 10
 
 	n := len(nums)
-	count := make([]int, n)
-	count[0] = 1
-
-	for length := 1; length < n; length++ {
-		for i := length; i > 0; i-- {
-			count[i] = (count[i] + count[i-1]) % MOD
+	for length := n; length > 1; length-- {
+		for i := 0; i < length-1; i++ {
+			nums[i] = (nums[i] + nums[i+1]) % MOD
 		}
 	}
-
-	res := 0
-	for i := 0; i < n; i++ {
-		res += (nums[i] * count[i]) % MOD
-	}
-	return res % MOD
+	return nums[0]
 }
