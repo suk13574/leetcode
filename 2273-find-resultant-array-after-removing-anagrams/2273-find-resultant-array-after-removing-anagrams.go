83b666c6b92c0acaff6579d251fe6a04c0f00f2c
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-10-13 09:35:36 +0900

    Time: 0 ms (100%), Space: 5 MB (96.97%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.go
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.go
@@ -1,25 +1,22 @@
-func isAnagram(w1, w2 string) bool {
-	if len(w1) != len(w2) {
-		return false
+func countAlphabet(w string) [26]byte {
+	count := [26]byte{}
+	for i := 0; i < len(w); i++ {
+		count[w[i]-'a']++
 	}
-
-	b1, b2 := []byte(w1), []byte(w2)
-	sort.Slice(b1, func(i, j int) bool { return b1[i] < b1[j] })
-	sort.Slice(b2, func(i, j int) bool { return b2[i] < b2[j] })
-
-	return string(b1) == string(b2)
+	return count
 }
 
 func removeAnagrams(words []string) []string {
 	res := []string{words[0]}
+	cntW1 := countAlphabet(res[0])
 	for i := 1; i < len(words); i++ {
-		w1 := res[len(res)-1]
 		w2 := words[i]
+		cntW2 := countAlphabet(w2)
 
-		if isAnagram(w1, w2) {
-			continue
+		if cntW1 != cntW2 {
+			res = append(res, w2)
+			cntW1 = cntW2
 		}
-		res = append(res, w2)
 	}
 
 	return res
