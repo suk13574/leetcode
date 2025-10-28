0a05715e2b4d0d4084a69c84adccc610e0331897
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2025-10-28 09:44:14 +0900

    Time: 0 ms (100%), Space: 4.2 MB (93.1%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.go
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.go
@@ -1,27 +1,27 @@
 func countValidSelections(nums []int) int {
-	n := len(nums)
-
 	total := 0
-	for i := 0; i < n; i++ {
-		total += nums[i]
+	for _, v := range nums {
+		total += v
 	}
 
-	left := 0
-	right := total
+	sumLeft := 0
 	res := 0
-	for i := 0; i < n; i++ {
-		if nums[i] > 0 {
-			left += nums[i]
-			right -= nums[i]
-			continue
+
+	for i := 0; i < len(nums); i++ {
+		sumRight := total - sumLeft
+
+		if nums[i] == 0 {
+			switch diff := sumLeft - sumRight; diff {
+			case 0:
+				res += 2
+			case 1, -1:
+				res += 1
+
+			}
 		}
 
-		if left == right {
-			res += 2
-		} else if left+1 == right || right+1 == left {
-			res += 1
-		}
-
+		sumLeft += nums[i]
 	}
+
 	return res
 }
