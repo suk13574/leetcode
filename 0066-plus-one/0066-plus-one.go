32d1f3f9e90724046d73e74e5324ab39710d3deb
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-01-01 10:36:36 +0900

    Time: 0 ms (100%), Space: 4.2 MB (3%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.go
@@ -0,0 +1,14 @@
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i]++
+			return digits
+		}
+	}
+
+	res := make([]int, len(digits)+1)
+	res[0] = 1
+	return res
+}
