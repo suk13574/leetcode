7f26ed80dae8093b9b1badcc533bda6896bbf4d5
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-01-02 09:25:56 +0900

    Time: 0 ms (100%), Space: 8.6 MB (66.67%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.go
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.go
@@ -1,23 +1,12 @@
-import "sort"
-
 func repeatedNTimes(nums []int) int {
 	n := len(nums) / 2
-
-	sort.Ints(nums)
-
-	res := 0
-	count := 0
-	for i := 0; i < len(nums); i++ {
-		if res == nums[i] {
-			count++
-		} else {
-			res = nums[i]
-			count = 1
+	seen := make(map[int]bool, n+1)
+	for _, n := range nums {
+		if seen[n] {
+			return n
 		}
 
-		if count == n {
-			return res
-		}
+		seen[n] = true
 	}
 
 	return 0
