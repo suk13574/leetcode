914e74560ac9d872adf6057548ac3cf2f1b303f8
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-02-26 09:38:02 +0900

    Time: 0 ms (100%), Space: 4 MB (44.44%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.go
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.go
@@ -1,18 +1,28 @@
 func numSteps(s string) int {
-    n := 0
-    for i := 0; i < len(s); i++ {
-        n = (n << 1) | int(s[i]-'0')
-    }
+	b := []byte(s)
+	res := 0
+	for len(b) > 1 {
+		n := len(b)
 
-    res := 0
-    for n > 1 {
-        if n & 1 == 1 {
-            res++
-            n += 1
-        }
-        n >>= 1
-        res++
-    }
+		if b[n-1] == '1' {
+			res++
 
-    return res
+			for i := n - 1; i >= 0; i-- {
+				if b[i] == '0' {
+					b[i] = '1'
+					break
+				}
+				b[i] = '0'
+			}
+		}
+
+		if b[0] == '0' {
+			return res + len(b)
+		}
+
+		b = b[:n-1]
+		res++
+	}
+
+	return res
 }
