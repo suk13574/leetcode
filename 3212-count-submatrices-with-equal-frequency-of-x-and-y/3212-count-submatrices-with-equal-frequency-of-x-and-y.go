6d0848263568132f9da0d61522d539f6c9202a72
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-03-19 09:38:45 +0900

    Time: 23 ms (50%), Space: 22.8 MB (50%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.go
@@ -0,0 +1,39 @@
+func numberOfSubmatrices(grid [][]byte) int {
+	r, c := len(grid), len(grid[0])
+
+	xPrefix := make([][]int, r+1)
+	yPrefix := make([][]int, r+1)
+
+	for i := 0; i < r+1; i++ {
+		xPrefix[i] = make([]int, c+1)
+		yPrefix[i] = make([]int, c+1)
+	}
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			v := grid[i][j]
+			xPrefix[i+1][j+1] = xPrefix[i+1][j] + xPrefix[i][j+1] - xPrefix[i][j]
+			yPrefix[i+1][j+1] = yPrefix[i+1][j] + yPrefix[i][j+1] - yPrefix[i][j]
+
+			if v == 'X' {
+				xPrefix[i+1][j+1]++
+			} else if v == 'Y' {
+				yPrefix[i+1][j+1]++
+			}
+		}
+	}
+
+	res := 0
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			xCnt := xPrefix[i+1][j+1]
+			yCnt := yPrefix[i+1][j+1]
+
+			if xCnt > 0 && xCnt == yCnt {
+				res++
+			}
+		}
+	}
+
+	return res
+}
