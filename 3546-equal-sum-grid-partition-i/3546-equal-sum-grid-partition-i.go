c5e0f2d5fac22c61384a2a36e4d0cbdaf704c6c8
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-03-25 09:53:46 +0900

    Time: 1 ms (100%), Space: 18 MB (75%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.go
@@ -0,0 +1,52 @@
+func canPartitionGrid(grid [][]int) bool {
+	r, c := len(grid), len(grid[0])
+
+	hsum := make([]int64, r)
+	vsum := make([]int64, c)
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			v := int64(grid[i][j])
+
+			hsum[i] += v
+			vsum[j] += v
+		}
+	}
+
+	for i := 1; i < r; i++ {
+		hsum[i] += hsum[i-1]
+	}
+	for j := 1; j < c; j++ {
+		vsum[j] += vsum[j-1]
+	}
+
+	if hsum[r-1]&1 == 0 {
+		target := hsum[r-1] / 2
+
+		for i := 0; i < r; i++ {
+			if target == hsum[i] {
+				return true
+			}
+
+			if target < hsum[i] {
+				break
+			}
+		}
+	}
+
+	if vsum[c-1]&1 == 0 {
+		target := vsum[c-1] / 2
+
+		for j := 0; j < c; j++ {
+			if target == vsum[j] {
+				return true
+			}
+
+			if target < vsum[j] {
+				break
+			}
+		}
+	}
+
+	return false
+}
