7a59a2b2e91f059ec32e8d0f01bc7386142e604b
Hyesook Yun <74169420+suk13574@users.noreply.github.com>
2026-09-22 09:26:08 +0900

    Time: 0 ms (100%), Space: 3.9 MB (99%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.go
@@ -0,0 +1,9 @@
+func checkOverlap(radius int, xCenter int, yCenter int, x1 int, y1 int, x2 int, y2 int) bool {
+    closestX := max(x1, min(xCenter, x2))
+    closestY := max(y1, min(yCenter, y2))
+
+    dx := xCenter - closestX
+    dy := yCenter - closestY
+
+    return dx*dx+dy*dy <= radius*radius
+}
